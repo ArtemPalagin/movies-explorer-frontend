01339d05162bd1694152b70ef639fb521f026995
ArtemPalagin <art.palagin@gmail.com>
2021-11-30 16:47:47 +0300

    Update Movies.js

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Movies.css';
 import MoviesCardList from '../MoviesCardList/MoviesCardList.js';
 import Preloader from '../Preloader/Preloader.js';
-import searchImage from '../../images/search-image.svg';
+import SearchForm from '../SearchForm/SearchForm.js';
+
 class Movies extends React.Component {
   constructor(props) {
     super(props);
@@ -10,18 +11,7 @@ class Movies extends React.Component {
   render() {
     return (
       <section className="movies">
-        <div className="movies__search">
-          <div className="movies__search-wrapper">
-            <input className="movies__inpute" placeholder="Фильм" type="text" name="search" />
-            <img className="movies__search-image" src={searchImage} alt="Не получилось загрузить иконку поиска" />
-          </div>
-          <div className="movies__switch-wrapper">
-            <p className="movies__text">Короткометражки</p>
-            <div className="movies__switch">
-              <input className="movies__switch-checkbox" type="checkbox" name="switcher" />
-            </div>
-          </div>
-        </div>
+        <SearchForm />
         <MoviesCardList cards={this.props.cards} />
         <Preloader />
         <button className="movies__button">Ещё</button>
