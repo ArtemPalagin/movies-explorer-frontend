bc210e9fb285b804057c3013239d156bb0d1242b
ArtemPalagin <art.palagin@gmail.com>
2021-11-30 16:46:44 +0300

    Update HeaderLogged.js

--- src/components/HeaderLogged/HeaderLogged.js
+++ src/components/HeaderLogged/HeaderLogged.js
@@ -10,7 +10,7 @@ class HeaderLogged extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      NavigationClosed: false,
+      NavigationClosed: true,
     }
   }
   CloseNavigation = () => {
