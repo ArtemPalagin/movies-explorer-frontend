77a204b06c41d2e9c5a9e7c8ac1deb658fae7c26
ArtemPalagin <art.palagin@gmail.com>
2021-12-02 10:47:48 +0300

    Update SavedMovies.js

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import '../Movies/Movies.css';
-
 import MoviesCardList from '../MoviesCardList/MoviesCardList.js';
-import searchImage from '../../images/search-image.svg';
+import SearchForm from '../SearchForm/SearchForm.js';
+
 class SavedMovies extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <section className=" saved-movies movies">
-        <div className="movies__search">
-          <div className="movies__search-wrapper">
-            <input className="movies__inpute" placeholder="Фильм" type="text" name="search" />
-            <img className="movies__search-image" src={searchImage} alt="Не получилось загрузить иконку поиска" />
-          </div>
-          <div className="movies__switch-wrapper">
-            <p className="movies__text">Короткометражки</p>
-            <div className="movies__switch">
-              <input className="movies__switch-checkbox" type="checkbox" name="switcher" />
-            </div>
-          </div>
-        </div>
+      <section className="saved-movies movies">
+        <SearchForm />
         <MoviesCardList cards={this.props.cards} />
       </section>
     )
