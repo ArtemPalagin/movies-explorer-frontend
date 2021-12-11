67f0f9a8da00caadcd07f907f392b355b5f96890
ArtemPalagin <art.palagin@gmail.com>
2021-12-11 13:55:01 +0300

    arrayCheck

--- src/utils/loadingController.js
+++ src/utils/loadingController.js
@@ -1,5 +1,4 @@
-const arrayCheck = (firstNumber, cardsToAdd, array) => {
-  const wantedLength = firstNumber + cardsToAdd
+const arrayCheck = (wantedLength, array) => {
   const filteredArray = array.slice(0, wantedLength);
   return {
     filteredArray,
@@ -13,60 +12,60 @@ export const download = (array, arrayNumber) => {
     if (arrayNumber % 4 !== 0) {
       const cardsToAdd = arrayNumber + (4 - (arrayNumber % 4))
       if(array.length <= cardsToAdd){
-        return arrayCheck(0, array.length, array);
+        return arrayCheck(array.length, array);
       }
-      return arrayCheck(0, cardsToAdd, array);
+      return arrayCheck(cardsToAdd, array);
     }
     if (arrayNumber > 16) {
-      return arrayCheck(0, arrayNumber, array);
+      return arrayCheck(arrayNumber, array);
     }
-    return arrayCheck(0, 16, array);
+    return arrayCheck(16, array);
   }
   if (window.innerWidth >= 768 && window.innerWidth < 930) {
     if (arrayNumber % 3 !== 0) {
       const cardsToAdd = arrayNumber + (3 - (arrayNumber % 3))
       if(array.length <= cardsToAdd){
-        return arrayCheck(0, array.length, array);
+        return arrayCheck(array.length, array);
       }
-      return arrayCheck(0, cardsToAdd, array);
+      return arrayCheck(cardsToAdd, array);
     }
     if (arrayNumber > 12) {
-      return arrayCheck(0, arrayNumber, array);
+      return arrayCheck(arrayNumber, array);
     }
-    return arrayCheck(0, 12, array);
+    return arrayCheck(12, array);
   }
   if (window.innerWidth >= 430 && window.innerWidth < 768) {
     if (arrayNumber % 2 !== 0) {
       const cardsToAdd = arrayNumber + (2 - (arrayNumber % 2))
       if(array.length <= cardsToAdd){
-        return arrayCheck(0, array.length, array);
+        return arrayCheck(array.length, array);
       }
-      return arrayCheck(0, cardsToAdd, array);
+      return arrayCheck(cardsToAdd, array);
     }
     if (arrayNumber > 8) {
-      return arrayCheck(0, arrayNumber, array);
+      return arrayCheck(arrayNumber, array);
     }
-    return arrayCheck(0, 8, array);
+    return arrayCheck(8, array);
   }
   if (window.innerWidth < 430) {
     if (arrayNumber > 5) {
-      return arrayCheck(0, arrayNumber, array);
+      return arrayCheck(arrayNumber, array);
     }
-    return arrayCheck(0, 5, array);
+    return arrayCheck(5, array);
   }
 }
 export const additionalDownload = (array, arrayNumber) => {
   if (window.innerWidth >= 903) {
-    return arrayCheck(arrayNumber, 4, array);
+    return arrayCheck(arrayNumber + 4, array);
   }
   if (window.innerWidth >= 768 && window.innerWidth < 930) {
-    return arrayCheck(arrayNumber, 3, array);
+    return arrayCheck(arrayNumber + 3, array);
   }
   if (window.innerWidth >= 430 && window.innerWidth < 768) {
-    return arrayCheck(arrayNumber, 2, array);
+    return arrayCheck(arrayNumber + 2, array);
   }
   if (window.innerWidth < 430) {
-    return arrayCheck(arrayNumber, 1, array);
+    return arrayCheck(arrayNumber + 1, array);
   }
 
 }
